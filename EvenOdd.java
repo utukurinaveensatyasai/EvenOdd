7efc23c9bc104b2edc0675ea9c8bbf7175d0c50d
Utukuri Naveen Satya Sai <132331077+utukurinaveensatyasai@users.noreply.github.com>
2024-07-29 14:15:25 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.evenodd;
+
+/**
+ *
+ * @author Naveen
+ */
+public class EvenOdd {
+
+    public static void main(String[] args) {
+        int num=14;
+        if(num%2==0)
+        {
+            System.out.println(num+" is Even Number");
+        }
+        else
+        {
+            System.out.println(num+" is Odd Number");
+        }
+    }
+}
